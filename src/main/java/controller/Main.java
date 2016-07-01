9a4f629d5249681c6bed13250d7e82345ab1ed54
Andrei_Zanozin <andrei_zanozin@epam.com>
2016-07-01 22:51:44 +0300

    Log ok

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -1,6 +1,7 @@
 package main.java.controller;
 
 import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 
 /**
  * Created by Andrei_Zanozin on 7/1/2016.
@@ -8,5 +9,7 @@ import org.apache.log4j.BasicConfigurator;
 public class Main {
     public static void main(String[] args) {
             BasicConfigurator.configure();
+        Logger log = Logger.getLogger(Main.class);
+        log.info("Hren");
         }
 }
