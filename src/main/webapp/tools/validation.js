a0d45097501a0988a8b5b21e77f7758154cfb40f
Andrei_Zanozin <andrei_zanozin@epam.com>
2016-07-13 16:46:06 +0300

    Correct registration page validation

--- src/main/webapp/tools/validation.js
+++ src/main/webapp/tools/validation.js
@@ -1,6 +1,8 @@
 var FILL_THIS = "<div class='paper'>This field must be filled</div>";
-var LOGIN_VALUE = "<div class='paper'>Please, enter a valid username</div>";
+var LOGIN_VALUE = "<div class='paper'>Please, enter a valid login</div>";
 var CONCIDENCED_FIELDS = "<div class='paper'>Fields must be different</div>";
+var MUST_MATCH = "<div class='paper'>Passwords must match</div>";
+var PASSWORD_LENGTH = "<div class='paper'>The password must be longer than 10 characters</div>";
 
 function validate() {
     clear();
@@ -9,34 +11,59 @@ function validate() {
     var required = $('.required');
     var login = $('.login');
     var coincidence = $('.coincidence');
+    var passwords = $('.password');
 
     for (var i = required.length - 1; i >= 0; i--) {
         if(!check_fill(required[i].value)){
             var tmp = required[i];
             if (tmp != null){
-                $(tmp).parent().append(FILL_THIS);
+                show(tmp, FILL_THIS);
                 allright = false;
             }
         }
     }
-    if(login != null && login[0].value != ""){
+    if(login != null && login.length > 0 && login[0].value != ""){
         if(!check_login_value(login[0].value)){
-            $(login[0]).parent().append(LOGIN_VALUE);
+            show(login[0], LOGIN_VALUE);
             allright = false;
         }else{
-            if(check_coincidence(coincidence)){
-                for (var i = coincidence.length - 1; i >= 0; i--) {
-                    $(coincidence[i]).parent().append(CONCIDENCED_FIELDS);
-                    allright = false;
+            if (coincidence.length > 0){
+                if(check_coincidence(coincidence)){
+                    for (var i = coincidence.length - 1; i >= 0; i--) {
+                        show(coincidence[i], CONCIDENCED_FIELDS);
+                        allright = false;
+                    }
+                }else{
+                    if (!check_empty_values(passwords)){
+                        if (!check_coincidence(passwords)){
+                            for (var i = passwords.length - 1; i >= 0; i--) {
+                                show(passwords[i], MUST_MATCH); 
+                            }
+                            allright = false;
+                        }else{
+                            if (!check_length(passwords[0].value, 10)){
+                                show(passwords[0], PASSWORD_LENGTH);
+                                allright = false;
+                            }
+                        }
+                    }
                 }
-        }
+            }
         }
     }
     return allright;
 }
 
+function show(child, message){
+    $(child).parent().append(message);
+    $(".paper").fadeIn(200);
+}
+
 function check_coincidence(args){
     for (var i = 0; i < args.length - 1; i++) {
+        if (args[i].value === ""){
+            return false;
+        }
         for (var j = i + 1; j < args.length; j++) {
             if (_coincidence(args[i].value, args[j].value)){
                 return true;
@@ -64,7 +91,20 @@ function check_login_value(login){
 }
 
 function check_fill(required) {
-    if (required != null && required === ""){
+    return required != "";
+}
+
+function check_empty_values(lits){
+    for (var i = lits.length - 1; i >= 0; i--) {
+        if (lits[i].value === ""){
+            return true;
+        }
+    }
+    return false;
+}
+
+function check_length(string, length){
+    if (string.length < length){
         return false;
     }else{
         return true;
@@ -72,5 +112,6 @@ function check_fill(required) {
 }
 
 function clear(){
+    $(".paper").fadeOut(200);
     $("div.paper").remove();
 }
